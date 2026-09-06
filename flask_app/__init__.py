f79ed03af196b0fc566389aef24c1315977f46bb
KennyLincoln <153145452+KennyLincoln@users.noreply.github.com>
2026-09-06 02:29:55 -0300

    security: remove hardcoded Flask secret

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,8 @@
+import os
+import secrets
+
 from flask import Flask
 
-app = Flask(__name__) 
 
-app.secret_key = "23232321111"
+app = Flask(__name__)
+app.secret_key = os.getenv("FLASK_SECRET_KEY") or secrets.token_hex(32)
